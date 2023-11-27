620702b514855406fa1139a3df0b84c6523dc25e
Matt <matiasgbraconi@gmail.com>
2023-11-27 21:04:18 +0100

    HostVan Links added
    minor fix in css
    
    Vans page searchParams implemented to navigate with querys

---
+++ pages/Host/HostVanInfo.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import { useOutletContext } from "react-router-dom"
+
+export default function HostVanInfo() {
+    const { currentVan } = useOutletContext()
+    return (
+        <section className="host-van-detail-info">
+            <h4>Name: <span>{currentVan.name}</span></h4>
+            <h4>Category: <span>{currentVan.type}</span></h4>
+            <h4>Description: <span>{currentVan.description}</span></h4>
+            <h4>Visibility: <span>public</span></h4>
+        </section>
+    )
+}
+
+// // {
+//     id: "1", name: "Modest Explorer", 
+//     price: 60, 
+//     description: "The Modest Explorer is a van designed to get you out of the 
+//     house and into nature. This beauty is equipped with solar panels, a 
+//     composting toilet, a water tank and kitchenette. The idea is that you can 
+//     pack up your home and escape for a weekend or even longer!", 
+//     imageUrl: "https://assets.scrimba.com/advanced-react/react-router/modest-explorer.png", 
+//     type: "simple", 
+//     hostId: "123"}
--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -1,8 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 export default function Vans() {
     const [vans, setVans] = React.useState([])
+    const [searchParams, setSearchParams] = useSearchParams()
+   
+    const typeFilter = searchParams.get("type")
+
 
     React.useEffect(() => {
         fetch("/api/vans")
@@ -10,7 +14,11 @@ export default function Vans() {
             .then(data => setVans(data.vans))
     }, [])
 
-    const vanElements = vans.map(van => (
+    const displayedVans = typeFilter 
+        ? vans.filter(van => van.type === typeFilter)
+        : vans
+
+    const vanElements =  displayedVans.map(van => (
         <div key={van.id} className="van-title">
             <Link to={`/vans/${van.id}`}>  
                 <img src={van.imageUrl} />
@@ -23,9 +31,54 @@ export default function Vans() {
         </div>
     ))
 
+    function handleFilterChange(key, value) {
+        setSearchParams(prevParams => {
+            if (value === null) {
+                prevParams.delete(key)
+            } else {
+                prevParams.set(key, value)
+            }
+            return prevParams
+        })
+    }
+
     return (
             <div className="van-list-container">
                 <h1>Explore our van options</h1>
+                <div className="van-list-filter-buttons">
+                    <button 
+                        onClick={() => handleFilterChange("type", "simple")}
+                        className={
+                            `van-type simple ${typeFilter === "simple" ? "selected" : ""}`
+                        } 
+                    >
+                        Simple
+                    </button>
+                    <button 
+                        onClick={() => handleFilterChange("type", "luxury")}
+                        className={
+                            `van-type luxury ${typeFilter === "luxury" ? "selected" : ""}`
+                        }
+                    >
+                        Luxury
+                    </button>
+                    <button 
+                        onClick={() => handleFilterChange("type", "rugged")}
+                        className={
+                            `van-type rugged ${typeFilter === "rugged" ? "selected" : ""}`
+                        } 
+                    >
+                        Rugged
+                    </button>
+                    {typeFilter ? (
+                        <button 
+                            onClick={() => handleFilterChange("type", null)}
+                            className="van-type clear-filters" 
+                        >
+                            Clear filters</button>
+                    ) : null}
+                    
+                </div>
                 <div className="van-list">
                     {vanElements}
                 </div>
