bcfd169bd0b2a67d7c7b3eaf4180cdd5bebc6f3a
Matt <matiasgbraconi@gmail.com>
2024-01-18 17:33:52 +0100

    Placehoalders out
    Dashboard, reviews and incomes placeholders are gone.

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React from "react"
 import { Link, NavLink } from "react-router-dom"
+import imageUrl from "/assets/images/avatar-icon.png"
 
 export default function Header() {
-    const activeStyle = {
+    const activeStyles = {
         fontWeight: "bold",
         textDecoration: "underline",
         color: "#161616"
@@ -11,37 +12,37 @@ export default function Header() {
     function fakeLogOut() {
         localStorage.removeItem("loggedin")
     }
-    
+
     return (
         <header>
             <Link className="site-logo" to="/">#VanLife</Link>
             <nav>
-                <NavLink 
+                <NavLink
                     to="/host"
-                    style={({isActive}) => isActive ? activeStyle : null}
+                    style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     Host
                 </NavLink>
-                <NavLink 
+                <NavLink
                     to="/about"
-                    style={({isActive}) => isActive ? activeStyle : null}
+                    style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     About
                 </NavLink>
-                <NavLink 
+                <NavLink
                     to="/vans"
-                    style={({isActive}) => isActive ? activeStyle : null}
+                    style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     Vans
                 </NavLink>
                 <Link to="login" className="login-link">
-                    <img 
-                        src="../assets/images/avatar-icon.png"
+                    <img
+                        src={imageUrl}
                         className="login-icon"
                     />
                 </Link>
                 <button onClick={fakeLogOut}>X</button>
             </nav>
         </header>
-        )
+    )
 }
--- components/HostLayout.jsx
+++ components/HostLayout.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import { NavLink, Outlet } from "react-router-dom"
 
 export default function HostLayout() {
-    const activeStyle = {
+    const activeStyles = {
         fontWeight: "bold",
         textDecoration: "underline",
         color: "#161616"
@@ -11,28 +11,30 @@ export default function HostLayout() {
     return (
         <>
             <nav className="host-nav">
-                <NavLink 
+                <NavLink
                     to="."
                     end
-                    style={({isActive}) => isActive ? activeStyle : null}
+                    style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     Dashboard
                 </NavLink>
-                <NavLink 
+
+                <NavLink
                     to="income"
-                    style={({isActive}) => isActive ? activeStyle : null}
+                    style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     Income
                 </NavLink>
-                <NavLink 
+                
+                <NavLink
                     to="vans"
-                    style={({isActive}) => isActive ? activeStyle : null}
+                    style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     Vans
                 </NavLink>
-                <NavLink 
+                <NavLink
                     to="reviews"
-                    style={({isActive}) => isActive ? activeStyle : null}
+                    style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     Reviews
                 </NavLink>
